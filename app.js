ebe52c9d977b17072f35fe4be2f8e71b7d8f329d
Kim Rautiainen <kim.rautiainen@hotmail.com>
2025-02-13 23:11:04 +0200

    eslint and prettier configured

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-require("dotenv").config();
-const express = require("express");
-const session = require("express-session");
-const passport = require("passport");
+require('dotenv').config();
+const express = require('express');
+const session = require('express-session');
+const passport = require('passport');
 require('./config/passport'); // Import passport configuration
 const app = express();
 
@@ -12,14 +12,14 @@ app.use(
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
-   // cookie: { secure: true }, // Set 'secure' to true if using HTTPS
+    // cookie: { secure: true }, // Set 'secure' to true if using HTTPS
   })
 );
 app.use(passport.initialize());
 app.use(passport.session());
 
 // Define routes
-app.use("/auth", require("./routes/auth"));
+app.use('/auth', require('./routes/auth'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
--- routes/auth.js
+++ routes/auth.js
@@ -4,11 +4,18 @@ const passport = require('passport');
 const router = express.Router();
 
 // Initiate Google authentication
-router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+router.get(
+  '/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] })
+);
 
 // Handle callback and redirect
-router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/' }), (req, res) => {
-  res.redirect('/dashboard');
-});
+router.get(
+  '/google/callback',
+  passport.authenticate('google', { failureRedirect: '/' }),
+  (req, res) => {
+    res.redirect('/dashboard');
+  }
+);
 
 module.exports = router;
