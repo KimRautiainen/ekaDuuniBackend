ddc003a181c0b4cb5ad036c5bba4be6eae81e453
Emil Lehtonen <emil.lehikoinen@outlook.com>
2025-03-01 17:51:39 +0200

    added code for this to work with azure database server

--- config/database.js
+++ config/database.js
@@ -1,12 +1,32 @@
 const { Sequelize } = require('sequelize');
+const { MsSqlDialect } = require('@sequelize/mssql');
+
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
-    dialect: 'mysql',
+    port: process.env.DB_PORT,
+    dialect: MsSqlDialect,
+    dialectOptions: {
+      options: {
+        encrypt: true,
+        enableArithAbort: true,
+      },
+    },
   }
 );
+
+async function testConnection() {
+  try {
+    await sequelize.authenticate();
+    console.log('Yhteys tietokantaan onnistui.');
+  } catch (error) {
+    console.error('Yhteyden muodostaminen epäonnistui:', error);
+  }
+}
+
+testConnection();
 
 module.exports = sequelize;
