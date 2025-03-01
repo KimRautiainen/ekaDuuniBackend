8279a9a9d90fa9546b7f21008b467ba2db37ae04
Emil Lehtonen <emil.lehikoinen@outlook.com>
2025-03-01 18:15:25 +0200

    changes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
 const passport = require('passport');
+const sequelize = require('./config/database'); // Import Sequelize instance
 require('./config/passport'); // Import passport configuration
 const app = express();
 
@@ -21,7 +22,17 @@ app.use(passport.session());
 // Define routes
 app.use('/auth', require('./routes/auth'));
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// Test database connection and start server
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Yhteys tietokantaan onnistui.');
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error('Yhteyden muodostaminen epäonnistui:', err);
+    process.exit(1); // Exit the process with failure
+  });
