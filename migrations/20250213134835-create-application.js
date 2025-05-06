f7950a04689de8118c8bb31ad9f771ce59843ac9
Emil <emil.lehikoinen@outlook.com>
2025-05-06 12:23:42 +0300

    removed try catch

--- migrations/20250213134835-create-application.js
+++ migrations/20250213134835-create-application.js
@@ -2,59 +2,54 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    try {
-      await queryInterface.createTable('Applications', {
-        id: {
-          allowNull: false,
-          autoIncrement: true,
-          primaryKey: true,
-          type: Sequelize.INTEGER,
-        },
-        user_id: {
-          type: Sequelize.INTEGER,
-          allowNull: false,
-          references: { model: 'Users', key: 'id' },
-          onDelete: 'CASCADE',
-        },
-        job_id: {
-          type: Sequelize.INTEGER,
-          allowNull: false,
-          references: { model: 'Jobs', key: 'id' },
-          onDelete: 'CASCADE',
-        },
-        cover_letter: {
-          type: Sequelize.TEXT,
-          allowNull: true,
-        },
-        resume_link: {
-          type: Sequelize.STRING,
-          allowNull: true,
-        },
-        status: {
-          type: Sequelize.STRING,
-          allowNull: false,
-          defaultValue: 'pending',
-        },
-        applied_at: {
-          type: Sequelize.DATE,
-          allowNull: false,
-          defaultValue: Sequelize.literal('GETDATE()'),
-        },
-        createdAt: {
-          type: Sequelize.DATE,
-          allowNull: false,
-          defaultValue: Sequelize.literal('GETDATE()'),
-        },
-        updatedAt: {
-          allowNull: false,
-          type: Sequelize.DATE,
-          defaultValue: Sequelize.literal('GETDATE()'),
-        },
-      });
-    } catch (err) {
-      console.error('❌ Migration failed:', err);
-      throw err;
-    }
+    await queryInterface.createTable('Applications', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'Users', key: 'id' },
+        onDelete: 'CASCADE',
+      },
+      job_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'Jobs', key: 'id' },
+        onDelete: 'CASCADE',
+      },
+      cover_letter: {
+        type: Sequelize.TEXT,
+        allowNull: true,
+      },
+      resume_link: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      status: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        defaultValue: 'pending',
+      },
+      applied_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('GETDATE()'),
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('GETDATE()'),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('GETDATE()'),
+      },
+    });
   },
 
   async down(queryInterface, Sequelize) {
