4e3f632fa7b0a2699989a4e68122139b1d4e1616
Emil <emil.lehikoinen@outlook.com>
2025-05-06 14:05:07 +0300

    alter jobs do noting if mssql

--- migrations/20250329102910-alter-job-description-to-utf8mb4.js
+++ migrations/20250329102910-alter-job-description-to-utf8mb4.js
@@ -1,21 +1,31 @@
-"use strict";
+'use strict';
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.sequelize.query(`
-      ALTER TABLE jobs
-      MODIFY job_description TEXT
-      CHARACTER SET utf8mb4
-      COLLATE utf8mb4_unicode_ci;
-    `);
+    const dialect = queryInterface.sequelize.getDialect();
+
+    if (dialect === 'mysql') {
+      // MySQL / MariaDB only
+      await queryInterface.sequelize.query(`
+        ALTER TABLE jobs
+          MODIFY job_description TEXT
+          CHARACTER SET utf8mb4
+          COLLATE utf8mb4_unicode_ci;
+      `);
+    }
+    // else on mssql we do nothing (or you could do a T-SQL ALTER COLUMN here)
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.sequelize.query(`
-      ALTER TABLE jobs
-      MODIFY job_description TEXT
-      CHARACTER SET utf8
-      COLLATE utf8_general_ci;
-    `);
+    const dialect = queryInterface.sequelize.getDialect();
+
+    if (dialect === 'mysql') {
+      await queryInterface.sequelize.query(`
+        ALTER TABLE jobs
+          MODIFY job_description TEXT
+          CHARACTER SET utf8
+          COLLATE utf8_general_ci;
+      `);
+    }
   }
 };
